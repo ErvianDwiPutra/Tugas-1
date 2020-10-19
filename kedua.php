25007b7afe9b7dfab07d5a8895b6a581535bf499
ErvianDwiPutra <71804207+ErvianDwiPutra@users.noreply.github.com>
2020-10-19 22:45:29 +0700

    Add files via upload

---
+++ kedua.php
@@ -0,0 +1,8 @@
+<?php
+	for ($i=1; $i <= 10; $i++) { 
+		echo "<br/> Hallo... Nama saya Ervian Dwi Putra";
+		echo "<br/> Saya Mahasiswa UNIGA, Prodi Sistem Informasi";
+		echo "<br/> Asal Nganjuk";
+		echo "<br/>";
+	}
+?>
